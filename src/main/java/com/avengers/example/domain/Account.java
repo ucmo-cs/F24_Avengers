56f10f37553beb78082ce05f0bc4eb6080f4283a
Zachary Pedigo <Zach.Pedigo@Oracle.com>
2024-10-14 09:25:13 -0500

    Added project files

---
+++ src/main/java/com/avengers/example/domain/Account.java
@@ -0,0 +1,98 @@
+package com.avengers.example.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Account
+{
+    @Id
+    private long acctId;
+    private int userType;
+    private String username;
+    private String password;
+    private String email;
+    private String phoneNumber;
+
+    public Account()
+    {
+    }
+
+    public Account(long acctId, int userType, String username, String password, String email, String phoneNumber)
+    {
+        this.acctId = acctId;
+        this.userType = userType;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setUserType(int userType)
+    {
+        this.userType = userType;
+    }
+
+    public int getUserType()
+    {
+        return userType;
+    }
+
+    public void setUsername(String username)
+    {
+        if (null == username || username == "" || username == " ")
+        {
+            throw new IllegalArgumentException("Username can not be null or empty!");
+        }
+        this.username = username;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setPassword(String password)
+    {
+        if (null == password || password == "" || password == " ")
+        {
+            throw new IllegalArgumentException("Password can not be null or empty!");
+        }
+        this.password = password;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setEmail(String email)
+    {
+        if (null == email || email == "" || email == " ")
+        {
+            throw new IllegalArgumentException("Email can not be null or empty!");
+        }
+        this.email = email;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setPhoneNumber(String phoneNumber)
+    {
+        if (null == phoneNumber || phoneNumber == "" || phoneNumber == " ")
+        {
+            throw new IllegalArgumentException("Phone Number can not be null or empty!");
+        }
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+}
