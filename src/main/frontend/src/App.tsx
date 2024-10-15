d273f3b5a382534932cfc5404e31d243c308f30d
Michael Harlow <94586121+michaelharlow@users.noreply.github.com>
2024-10-15 14:25:31 -0500

    frontend: init + shadcn/ui and data fetch

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -1,22 +1,62 @@
-import { useState } from 'react'
-import {Button} from "@/components/ui/button.tsx";
-import "@/index.css"
+import {useEffect, useState} from 'react';
+import {Button} from "@/components/ui/button";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+import "@/index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [accounts, setAccounts] = useState<Account[]>([]);
 
-  return (
-    <div className={"flex m-auto w-1/2 h-screen justify-center flex-col items-center"}>
-      <h1>Vite + React</h1>
-        <Button onClick={() => setCount((count) => count + 1)} variant={"destructive"}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  useEffect(() => {
+    console.log("mounted"); // TODO remove
+
+    fetch("http://localhost:8080/accounts")
+        .then(response => response.json())
+        .then(data => setAccounts(data))
+        .catch(error => console.error(error));
+
+    return () => {
+      console.log("unmounted"); // TODO remove
+    }
+  }, []);
+
+
+    return (
+    <div className={"flex m-auto w-fit h-screen justify-center flex-col items-center"}>
+        <h1>Commerce Bank Frontend</h1>
+        <Button  className="my-6">This does nothing</Button>
+        <Table>
+            <TableCaption>list of Accounts in DB</TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>Id</TableHead>
+                    <TableHead>Type</TableHead>
+                    <TableHead>username</TableHead>
+                    <TableHead>Password</TableHead>
+                    <TableHead>email</TableHead>
+                    <TableHead>phoneNumber</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {accounts.map((account) => (
+                    <TableRow key={account.acctId}>
+                        <TableCell>{account.acctId}</TableCell>
+                        <TableCell>{account.userType}</TableCell>
+                        <TableCell>{account.username}</TableCell>
+                        <TableCell>{account.password}</TableCell>
+                        <TableCell>{account.email}</TableCell>
+                        <TableCell>{account.phoneNumber}</TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
     </div>
   )
 }
