7b4242de7351b9b9b972647bac1d7da01c7aea77
Michael Harlow <94586121+michaelharlow@users.noreply.github.com>
2024-10-15 22:49:22 -0500

    frontend: routing w/ react router v6

--- src/main/frontend/src/main.tsx
+++ src/main/frontend/src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import Layout from "@/components/Layout.tsx";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path={"/"} element={<Layout />}>
+            <Route index element={<App />} /> {/* element={<Navigate to={"login"} replace />} */}
+            <Route path={"login"} element={<h1>login page</h1>} />
+        </Route>
+    )
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
