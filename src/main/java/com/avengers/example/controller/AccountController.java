71c92cf04f55ed415c88e47e0379d41ab299f9a6
elliottc32 <enc32690@ucmo.edu>
2024-11-03 13:44:47 -0600

    Added CrossOrigin to all Controllers globally.

--- src/main/java/com/avengers/example/controller/AccountController.java
+++ src/main/java/com/avengers/example/controller/AccountController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @AllArgsConstructor
 @RestController
 public class AccountController
