9939bfda724600e1c634a4e2459a118354c18ef8
Michael Harlow <94586121+michaelharlow@users.noreply.github.com>
2024-11-08 12:46:36 -0600

    bug: Loan format

--- src/main/java/com/avengers/example/config/TestDatabaseConfig.java
+++ src/main/java/com/avengers/example/config/TestDatabaseConfig.java
@@ -4,7 +4,6 @@ import com.avengers.example.domain.Account;
 import com.avengers.example.domain.Loan;
 import com.avengers.example.repository.AccountRepository;
 import com.avengers.example.repository.LoanRepository;
-import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
--- src/main/java/com/avengers/example/domain/Loan.java
+++ src/main/java/com/avengers/example/domain/Loan.java
@@ -1,7 +1,5 @@
 package com.avengers.example.domain;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
