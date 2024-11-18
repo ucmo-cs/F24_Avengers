8e27ac29a011e788e78667dbca50e76203a138a1
Michael Harlow <94586121+michaelharlow@users.noreply.github.com>
2024-11-18 12:10:47 -0600

    frontend: api extraction

--- src/main/frontend/src/lib/api.ts
+++ src/main/frontend/src/lib/api.ts
@@ -1,11 +1,17 @@
-export function getLoans() {
-    return fetch("/api/loans")
+export async function getLoans() {
+    return await fetch("/api/loans")
         .then((response) => response.json())
         .catch((error) => console.error(error));
 }
 
 export function getAccounts() {
     return fetch("/api/accounts")
+        .then((response) => response.json())
+        .catch((error) => console.error(error));
+}
+
+export function getAccountLoans(id: string | undefined) {
+    return fetch(`/api/account/${id}/loans`)
         .then((response) => response.json())
         .catch((error) => console.error(error));
 }
